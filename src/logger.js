42dbbd9e35b91edce34fc9d529b96cff99922bc9
Anton Fisher <a.fschr@gmail.com>
2019-09-29 12:00:55 -0700

    chore: update winston logger to v3

--- src/logger.js
+++ src/logger.js
@@ -1,33 +1,36 @@
 const path = require('path');
 const {homedir} = require('os');
-const {Logger, transports: {Console, File}} = require('winston');
+const {createLogger, format, transports} = require('winston');
 
 const LOG_FILE_NAME = '.flap-app-launcher.log';
-const LOG_FILE_PATH = (
+const LOG_FILE_PATH =
   process.env.NODE_ENV === 'production'
     ? path.join(homedir(), LOG_FILE_NAME)
-    : path.join(__dirname, '..', LOG_FILE_NAME)
-);
+    : path.join(__dirname, '..', LOG_FILE_NAME);
 
-const logger = new Logger({
-  transports: [
-    new Console({
-      level: 'debug',
-      colorize: true,
-      timestamp: true,
-      prettyPrint: true
+const transportConsole = new transports.Console({
+  level: 'debug'
+});
+const transportFile = new transports.File({
+  level: 'info',
+  filename: LOG_FILE_PATH,
+  tailable: true,
+  maxsize: 10 * 1024 * 1024,
+  maxFiles: 10
+});
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
     }),
-    new File({
-      filename: LOG_FILE_PATH,
-      handleExceptions: true,
-      humanReadableUnhandledException: true,
-      prettyPrint: true,
-      tailable: true,
-      maxsize: 10 * 1024 * 1024,
-      maxFiles: 10,
-      json: false
-    })
-  ]
+    format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+    format.errors({stack: true})
+  ),
+  transports: [transportConsole, transportFile],
+  exceptionHandlers: [transportConsole, transportFile]
 });
 
 logger.info(`Application logs file: ${LOG_FILE_PATH}`);
